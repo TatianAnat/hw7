aa369daaab4e609dd178bdf5cdc006b4bf6ec97b
TatianAnat <TatianAnat@yandex.ru>
2025-04-11 22:58:35 +0400

    work 7 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        byte clienOS = 0;
+        if (clienOS == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+                    } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
