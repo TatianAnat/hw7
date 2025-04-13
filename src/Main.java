2be8ebc490d958022b50f3cfeee1855f3d2ef858
TatianAnat <TatianAnat@yandex.ru>
2025-04-13 15:15:15 +0400

    work 7 task 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        //task1
         byte clienOS = 0;
         if (clienOS == 1) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-                    } else {
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+        //task2
+        byte clienOSNew = 0;
+        int clientDeviceYear = 2013;
+        if (clienOSNew == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+        if (clienOSNew == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+
     }
 }
