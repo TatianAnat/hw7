117cc1558ba9655f956275f8cb6223092987ae01
TatianAnat <TatianAnat@yandex.ru>
2025-04-13 15:43:52 +0400

    work 7 task 3

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,17 @@ public class Main {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
         }
-
+        //task3
+        int year = 1584;
+        if (year > 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+        }
+        else {
+            System.out.println("введите год больше 1584");
+        }
     }
 }
